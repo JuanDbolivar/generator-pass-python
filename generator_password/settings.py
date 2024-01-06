e26cee89d6ddfc09ffa30109e4d5886ece01ea5f
JuanD <juanxd3009@gmail.com>
2024-01-05 22:36:17 -0500

    .

--- generator_password/settings.py
+++ generator_password/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p#h=-l%5wom7w=gizeu0(f6*&v5pyz3gmio+szu0*3lls%-toe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
